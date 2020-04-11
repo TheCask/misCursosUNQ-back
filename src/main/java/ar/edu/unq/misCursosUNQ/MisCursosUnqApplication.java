82afd3003ee2de0ff527c2007998e721467c73d3
TheCask <eugeniocalcena@gmail.com>
2020-04-11 20:58:39 -0300

    first commit

---
+++ src/main/java/ar/edu/unq/misCursosUNQ/MisCursosUnqApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.misCursosUNQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisCursosUnqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisCursosUnqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unq/misCursosUNQ/MisCursosUnqApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.misCursosUNQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisCursosUnqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
