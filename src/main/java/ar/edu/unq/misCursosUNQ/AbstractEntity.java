8875573bbe2fccc2bf8ecf60d883e583e9f046af
Elias <Elias@DESKTOP-PJ5NOQK>
2020-04-12 19:35:03 -0300

    Primeras clases

---
+++ src/main/java/ar/edu/unq/misCursosUNQ/AbstractEntity.java
@@ -0,0 +1,18 @@
+package ar.edu.unq.misCursosUNQ;
+
+import java.io.Serializable;
+
+public abstract class AbstractEntity implements Serializable {
+ 
+	protected Long id;
+ 
+	// Este metodo es abstracto porque las anotaciones no son heredables 
+	public abstract Long getId();
+ 
+	// Este metodo es protegido para evitar que un prrgramador pueda poner un
+	// idenfificador en la instancia, ya que los identitificadores deben ser
+	// gestionados por la capa de persistencia
+	protected void setId(final Long id) {
+		this.id = id;
+	}
+}
