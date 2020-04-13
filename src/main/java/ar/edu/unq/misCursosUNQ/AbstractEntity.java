38b21fca5b21641f384a41e02373207e0b8fff29
TheCask <eugeniocalcena@gmail.com>
2020-04-12 21:12:50 -0300

    actualizacion pom

--- src/main/java/ar/edu/unq/misCursosUNQ/AbstractEntity.java
+++ src/main/java/ar/edu/unq/misCursosUNQ/AbstractEntity.java
@@ -9,7 +9,7 @@ public abstract class AbstractEntity implements Serializable {
 	// Este metodo es abstracto porque las anotaciones no son heredables 
 	public abstract Long getId();
  
-	// Este metodo es protegido para evitar que un prrgramador pueda poner un
+	// Este metodo es protegido para evitar que un programador pueda poner un
 	// idenfificador en la instancia, ya que los identitificadores deben ser
 	// gestionados por la capa de persistencia
 	protected void setId(final Long id) {
