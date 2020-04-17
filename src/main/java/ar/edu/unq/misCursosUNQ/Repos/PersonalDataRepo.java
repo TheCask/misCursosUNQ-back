a2271b907f0be4c38bb7a0ab8962f610888eb092
TheCask <eugeniocalcena@gmail.com>
2020-04-17 00:16:01 -0300

    personal data repo

---
+++ src/main/java/ar/edu/unq/misCursosUNQ/Repos/PersonalDataRepo.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.misCursosUNQ.Repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ar.edu.unq.misCursosUNQ.User;
+
+@Repository
+public interface PersonalDataRepo extends JpaRepository<User, Long> {
+
+	/* By this simple extension, MateriaRepo inherits several methods 
+	 * for working with Materia persistence, including methods for saving, deleting, and finding User entities.
+	 * Along with default provided methods, we can add our own custom methods and queries to this interface. */
+}
