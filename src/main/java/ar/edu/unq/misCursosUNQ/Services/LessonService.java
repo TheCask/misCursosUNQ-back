7c2d2e239ec705eb9b46dea4758f54d35c3b2bde
Elias <Elias@DESKTOP-PJ5NOQK>
2020-04-18 11:47:14 -0300

    Lesson Service

---
+++ src/main/java/ar/edu/unq/misCursosUNQ/Services/LessonService.java
@@ -0,0 +1,59 @@
+package ar.edu.unq.misCursosUNQ.Services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unq.misCursosUNQ.Lesson;
+import ar.edu.unq.misCursosUNQ.Subject;
+import ar.edu.unq.misCursosUNQ.Exceptions.RecordNotFoundException;
+import ar.edu.unq.misCursosUNQ.Repos.LessonRepo;
+ 
+//@Service
+public class LessonService {
+     
+    //@Autowired
+	LessonRepo repository;
+     
+    public List<Lesson> getLessons() {
+        List<Lesson> aList = repository.findAll();
+         
+        if(aList.size() > 0) { return aList; } 
+        else { return new ArrayList<Lesson>(); }
+    }
+     
+    public Lesson getLessonById(Long id) throws RecordNotFoundException {
+        Optional<Lesson> optional = repository.findById(id);
+         
+        if(optional.isPresent()) { return optional.get(); } 
+        else { throw new RecordNotFoundException("Lesson record does not exist for given id"); }
+    }
+     
+    public Lesson createOrUpdateSubject(Lesson entity) throws RecordNotFoundException {
+        
+    	Optional<Lesson> optEntity = repository.findById(entity.getLessonId());
+
+    	if(optEntity.isPresent()) {
+    		
+    		Lesson newEntity = optEntity.get();
+
+    		newEntity.setAttendantStudents(entity.getAttendantStudents());	
+
+    		newEntity = repository.save(newEntity);
+
+    		return newEntity;
+    	} 
+    	else { return repository.save(entity); }
+	        
+    } 
+     
+    public void deleteLessonById(Long id) throws RecordNotFoundException {
+        Optional<Lesson> optEntity = repository.findById(id);
+         
+        if(optEntity.isPresent()) { repository.deleteById(id); } 
+        else { throw new RecordNotFoundException("Lesson record does not exist for given id"); }
+    } 
+}
