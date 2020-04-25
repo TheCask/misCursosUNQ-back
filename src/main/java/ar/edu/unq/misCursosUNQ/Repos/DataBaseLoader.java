eb7f2f8edc4c808a0e2833723264e724856fb642
TheCask <eugeniocalcena@gmail.com>
2020-04-25 10:55:57 -0300

    more examples to db

--- src/main/java/ar/edu/unq/misCursosUNQ/Repos/DataBaseLoader.java
+++ src/main/java/ar/edu/unq/misCursosUNQ/Repos/DataBaseLoader.java
@@ -28,6 +28,11 @@ public class DataBaseLoader implements CommandLineRunner  {
 		aCourse.setCourseIsOpen(false);
 		aCourse.setCourseShift("Noche");
 		
+		Course otherCourse = new Course("EPYL-C3");
+		otherCourse.setCourseCode("80005-C3-CYT2");
+		otherCourse.setCourseIsOpen(true);
+		otherCourse.setCourseShift("Mañana");
+		
 		Student student1 = new Student("Eugenio", "Calcena", 28123123, "eugeniocalcena@gmail.com",14111);
 		Student student2 = new Student("Elias", "Filipponi", 29123456, "eliasfilipponi@gmail.com",14222);
 		Student student3 = new Student("Regina", "Falange",  33333333, "s3@gmail.com",14333);
@@ -37,6 +42,8 @@ public class DataBaseLoader implements CommandLineRunner  {
 		Lesson lesson1 = new Lesson(LocalDate.now());
 		Lesson lesson2 = new Lesson(LocalDate.now().plusDays(1));
 		Lesson lesson3 = new Lesson(LocalDate.now().plusDays(2));
+		Lesson lesson4 = new Lesson(LocalDate.now().plusDays(10));
+		Lesson lesson5 = new Lesson(LocalDate.now().plusDays(5));
 		
 		aCourse.addLesson(lesson1);
 		aCourse.addLesson(lesson2);
@@ -47,16 +54,20 @@ public class DataBaseLoader implements CommandLineRunner  {
 		aCourse.addStudent(student3);
 		aCourse.addStudent(student4);
 		aCourse.addStudent(student5);
-
-		
 		
 		lesson1.setAttendance(student1);
 		lesson2.setAttendance(student2);
 		lesson3.setAttendance(student1);
 		lesson3.setAttendance(student2);
 		
+		otherCourse.addLesson(lesson4);
+		otherCourse.addLesson(lesson5);
 		
+		otherCourse.addStudent(student1);
+		otherCourse.addStudent(student2);
+
 		this.csService.createOrUpdateCourse(aCourse);
+		this.csService.createOrUpdateCourse(otherCourse);
 	}
 	
 }
